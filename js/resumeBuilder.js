599799144504f559aaaab3369e130344717d6efc
Soham <soham5959595@gmail.com>
2017-01-18 12:01:16 +0530

    2nd commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,34 @@
-$("#main").append("Soham");
+var bio = {
+    "name" : "Soham Das",
+    "role" : "Student",
+    "contactInfo" : "9954783950",
+    "picture" : "images/me.png",
+    "welcomeMessage" : "This is my resume",
+    "skills" : ["C"," Java"," Javascript"," Web Development"," Android Development"]
+}
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contactInfo);
+var formattedbiopic = HTMLbioPic.replace("%data%", bio.picture);
+var formattedwelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedMobile);
+$("#header").prepend(formattedbiopic);
+$("#lets-connect").append(formattedwelcomeMessage);
+$("#header").append(formattedSkills);
+
+var work = {};
+var education = {};
+
+work.position = "Student";
+
+education["name"] = "RNSIT";
+education["years"] = "2015-";
+education["city"] = "Bengaluru";
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
